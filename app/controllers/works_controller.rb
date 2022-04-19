c22e05097a431b4bebeaa1e37c2eb06bf5ee5711
pamapama1 <pamapama125@gmail.com>
2022-04-19 22:01:28 +0900

    start_time実装

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,21 +1,20 @@
 class WorksController < ApplicationController
   def index
   end
-
+  def now_work
+  end
   def new
-    #@work = Work.new
-    @work_time = Time.now
+    @work = Work.new
   end
   def create
-  #  @work = Work.new(work_params)
-    binding.pry
+    #@work = Work.new(work_params)
    # @work_time = (Time.now-@start_time)
   end
 
   private
 
   def work_params
- #   params.require(:work).permit(start_time)
+    params.require(:work).permit(:start_time)
   end
 
 end
