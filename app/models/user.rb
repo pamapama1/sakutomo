03763479478bbfad6d45c538dd43e59c8759e692
pamapama1 <pamapama125@gmail.com>
2022-05-05 22:16:26 +0900

    確認用

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ validates :nickname, presence: true
 PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+{6,}\z/i.freeze
 validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字を含めて設定してください'
 
+
 end
